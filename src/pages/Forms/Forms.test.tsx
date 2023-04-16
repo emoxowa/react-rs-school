09eedd161e44e31fda3f44077c587e2229b77f39
Evgeniia <Kochetkovaev11@gmail.com>
2023-04-16 18:49:32 +0300

    feat: add tests

--- src/pages/Forms/Forms.test.tsx
+++ src/pages/Forms/Forms.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Forms from './Forms';
+import { Provider } from 'react-redux';
+import store from '../../store/store';
 
 test('renders Forms component', () => {
-  render(<Forms />);
+  render(
+    <Provider store={store}>
+      <Forms />
+    </Provider>
+  );
   const formsTitle = screen.getByText(
     'Add your healthy recipe and become an author in our recipe book!'
   );
